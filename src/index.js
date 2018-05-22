52dfe48b1109a4bb2251fed8abeb4da0d30b3dac
Jeremy Stern <jzstern@gmail.com>
2018-05-22 16:23:44 -0400

    following lecture vids

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import CourseManager from './containers/CourseManager'
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/font-awesome/css/font-awesome.css'
 
 ReactDOM.render(
-	<h1>Hello world!</h1>,
+	<div className="container-fluid">
+		<CourseManager/>
+	</div>,
 	document.getElementById('root')
-);  
+);
